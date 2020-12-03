cdf480be29661dd916a01c790dba5521a23c2916
mgechev123 <72758767+mgechev123@users.noreply.github.com>
2020-12-03 13:50:19 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from flask import Flask
 app = Flask(__name__)
  
 @app.route('/')
 def hello_world():
-    return ("Hello, World!"      
-           + "<br><br><br><br>Apresentacao trabalho final!"
-           + "<br><br><br><br>TESTE")
+     return 'a'
+ #   return ("Hello, World!"      
+  #         + "<br><br><br><br>Apresentacao trabalho final!"
+   #        + "<br><br><br><br>TESTE")
