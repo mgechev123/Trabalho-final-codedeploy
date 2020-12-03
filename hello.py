9b926324eab2c1b500603af9788ce1b8eb4b8564
mgechev123 <72758767+mgechev123@users.noreply.github.com>
2020-12-03 13:56:11 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def hello_world():
-     return 'aáãç'
- #   return ("Hello, World!"      
-  #         + "<br><br><br><br>Apresentacao trabalho final!"
-   #        + "<br><br><br><br>TESTE")
+     #return 'aáãç'
+    return ("Hello, World!"      
+           + "<br><br><br><br>Apresentação do trabalho final..."
+           + "<br><br><br><br>TESTE")
