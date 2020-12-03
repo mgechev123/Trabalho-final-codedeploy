0564d3350215dc9192fe82d1d9b5142217cc7e8c
mgechev123 <72758767+mgechev123@users.noreply.github.com>
2020-12-03 15:14:46 -0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -5,8 +5,6 @@ app = Flask(__name__)
  
 @app.route('/')
 def hello_world():
- #return unicode("　|Ｔ￣て￣￣`ー-<","UTF-8")
-return unicode(" ＿　　　　　　　　／","UTF-8")
-    #return ("Hello, World!"      
-     #      + "<br><br><br><br>Apresentação do trabalho final..."
-      #     + "<br><br><br><br>")
+    return ("Hello, World!"      
+           + "<br><br><br><br>Apresentação do trabalho final..."
+           + "<br><br><br><br>")
